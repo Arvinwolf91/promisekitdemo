c91b407da201124c5d969e2d206bfe179cc2dbe6
arvin.sanmuga <arvin_wolf@Arvins-MacBook-Pro-2.local>
2018-02-26 18:03:59 +0800

    [] Added model class for ObjectMapper

---
+++ promisekitdemo/Service/Contact.swift
@@ -0,0 +1,27 @@
+//
+//  Contact.swift
+//  promisekitdemo
+//
+//  Created by Arvin Sanmuga Rajah on 26/02/2018.
+//  Copyright © 2018 Arvin Sanmuga Rajah. All rights reserved.
+//
+
+//import UIKit
+//import ObjectMapper
+//
+//class Contact: Mappable {
+//    
+//    var name = ""
+//    var email = ""
+//    var gender = ""
+//    
+//    required init?(map: Map) {}
+//    
+//    func mapping(map: Map) {
+//        name <- map["name"]
+//        email <- map["email"]
+//        gender <- map["gender"]
+//    }
+//
+//}
+
